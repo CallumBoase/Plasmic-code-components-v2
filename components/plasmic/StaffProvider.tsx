dcab0e48586f79db7028c9e8f52ef2c9ed83a4e4
CallumBoase <callum.boase@gmail.com>
2023-12-05 13:06:13 +1100

    Staff state

--- components/plasmic/StaffProvider.tsx
+++ components/plasmic/StaffProvider.tsx
@@ -1,25 +1,43 @@
+import { useState, useEffect } from 'react';
 import { usePlasmicQueryData } from "@plasmicapp/loader-nextjs";
 import { DataProvider } from "@plasmicapp/loader-nextjs";
 import { createBrowserClient } from "@supabase/ssr";
 
-export function StaffProvider({ children }: { children: React.ReactNode }) {
-  const { data } = usePlasmicQueryData('/staff', async () => {
+interface StaffActions {
+  deleteStaff(id: number): void;
+}
 
+export function StaffProvider({ children }: { children: React.ReactNode }) {
+  const { data: fetchedData, error, isLoading } = usePlasmicQueryData("/staff", async () => {
     const supabase = createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+    );
 
-    const { data, error } = await supabase.from('staff').select('*');
+    const { data, error } = await supabase.from("staff").select("*");
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    if(error) throw error;
+    if (error) throw error;
 
     return data;
-
   });
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (fetchedData) {
+      setData(fetchedData);
+    }
+  }, [fetchedData]);
+
+  const deleteStaff = (id) => {
+    setData(currentData => currentData.filter(staff => staff.id !== id));
+  }
 
   return (
     <>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
       {data && (
         <DataProvider name="staff" data={data}>
           {children}
