b126750c4953c3d3a95a00b31a779c6ee4ab8970
CallumBoase <callum.boase@gmail.com>
2023-12-06 16:31:31 +1100

    Example middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log('hello from middleware')
+  if(Math.random() > 0.5) {
+    return NextResponse.next()
+  
+  } else {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+}
+ 
+// // See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/staff',
+}
