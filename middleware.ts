dfc4ce43044f7b4567504945fa3f58d486693bd3
CallumBoase <callum.boase@gmail.com>
2023-12-06 17:35:50 +1100

    middleware tidy-up

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,12 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   console.log('hello from middleware')
-  if(Math.random() > 0.5) {
+  // if(Math.random() > 0.5) {
     return NextResponse.next()
   
-  } else {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+  // } else {
+    // return NextResponse.redirect(new URL('/', request.url))
+  // }
 }
  
 // // See "Matching Paths" below to learn more
