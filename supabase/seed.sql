e3d5311bf8e5a06981b5dc9e5496a8250202f86f
CallumBoase <callum.boase@gmail.com>
2023-12-15 05:51:39 +1100

    Working on seeding

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,8 @@
+--create users
+-- select public.create_user('test@example.com', 'password');
+-- select public.create_user('tes2t@example.com', 'password');
+-- select public.create_user('test3@example.com', 'password');
+
 INSERT INTO business (name) VALUES
   ('PM Fresh'),
   ('Nuzest');
@@ -29,38 +34,10 @@ INSERT INTO country_on_market_group (market_group_id, country_id) VALUES
   (4, 5);
 
 INSERT INTO product (title, business_id) VALUES
-  ('A delicious salad', 1),
-  ('Coleslaw', 1),
-  ('Tofu salad', 1),
-  ('Vegemite', 2),
-  ('Milo', 2),
-  ('Tim Tams', 2);
+  ('Fruit Salad Watermelon Fingers', 1);
 
 INSERT INTO market_article (product_id, market_group_id) VALUES
-  (1, 1),
-  (1, 2),
-  (2, 1),
-  (2, 2),
-  (3, 1),
-  (3, 2),
-  (4, 3),
-  (4, 4),
-  (5, 3),
-  (5, 4),
-  (6, 3),
-  (6, 4);
+  (1, 1);
 
 INSERT INTO artwork_item (market_article_id, layer) VALUES
-  (1, 'Inner'),
-  (1, 'MPO'),
-  (2, 'Inner'),
-  (2, 'MPO'),
-  (3, 'Inner'),
-  (3, 'MPO'),
-  (4, 'Inner'),
-  (4, 'MPO'),
-  (5, 'Inner'),
-  (5, 'MPO'),
-  (6, 'Inner'),
-  (6, 'MPO');
-
+  (1, 'Single'); 
