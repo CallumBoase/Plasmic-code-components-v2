c34038d02a087f20f2a8377f26e888c82b5eb540
CallumBoase <callum.boase@gmail.com>
2024-02-06 11:27:59 +1100

    Works in both

--- components/SupabaseUserProvider.tsx
+++ components/SupabaseUserProvider.tsx
@@ -1,10 +1,11 @@
 import { useSafeRouter as useRouter } from "@/utils/useSafeRouter";
-import { DataProvider } from "@plasmicapp/loader-nextjs";
+import { DataProvider, PlasmicCanvasContext } from "@plasmicapp/loader-nextjs";
 import { GlobalActionsProvider } from "@plasmicapp/host";
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect, useCallback, useMemo, useContext } from "react";
 import supabaseBrowserClient from "@/utils/supabaseBrowserClient";
 import type { Json } from "@/types/supabase";
 import getErrMsg from "@/utils/getErrMsg";
+import type { AuthTokenResponse } from "@supabase/supabase-js";
 
 type User = {
   email: string | null;
@@ -14,147 +15,210 @@ type User = {
 
 interface DataProviderData {
   user: User | null;
-  simulateUserSettings: {
-    simulateLoggedInUser: boolean;
-    email: string | null;
-    password: string | null;
-  };
   error: string | null;
 }
 
 interface SupabaseUserComponentProps {
   children: React.ReactNode;
   redirectOnLoginSuccess?: string;
-  simulateLoggedInUser: boolean;
-  email: string | null;
-  password: string | null;
 }
 
-export const SupabaseUser = ({children, redirectOnLoginSuccess, simulateLoggedInUser, email, password}: SupabaseUserComponentProps) => {
+export const SupabaseUser = ({children, redirectOnLoginSuccess}: SupabaseUserComponentProps) => {
 
   //Nextjs router
   const router = useRouter();
+
+  // //Determine if we are in the Plasmic studio
+  // const inEditor = useContext(PlasmicCanvasContext) ? true : false;
+
+  // try {
+  //   window.localStorage.setItem('createdFromUserProvider', Math.random().toString());
+  // } catch(err){}
   
   //Setup state
-  const [session, setSession] = useState<User | null>(null);
+  const [session, setSession] = useState<AuthTokenResponse["data"]["session"] | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const [simulateUserSettings, setSimulateUserSettings] = useState({
-    simulateLoggedInUser: simulateLoggedInUser,
-    email: email,
-    password: password,
-  });
+  // const [simulateUserSettings, setSimulateUserSettings] = useState({
+  //   simulateLoggedInUser: simulateLoggedInUser,
+  //   email: email,
+  //   password: password,
+  // });
 
   //Update simulateUserSettings when props change
-  useEffect(() => {
-    setSimulateUserSettings({
-      simulateLoggedInUser: simulateLoggedInUser,
-      email: email,
-      password: password,
-    });
-  }, [simulateLoggedInUser, email, password]);
+  // useEffect(() => {
+  //   setSimulateUserSettings({
+  //     simulateLoggedInUser: simulateLoggedInUser,
+  //     email: email,
+  //     password: password,
+  //   });
+  // }, [simulateLoggedInUser, email, password]);
 
   //Callback to get the logged in user's session
-  const getSession = useCallback(async () => {
-    const supabase = await supabaseBrowserClient(simulateUserSettings);
+  // const getSession = useCallback(async () => {
+  //   const supabase = await supabaseBrowserClient(simulateUserSettings);
 
-    if (!simulateUserSettings.simulateLoggedInUser) {
-      //If we are NOT simulating logged in user, get session from localStorage / cookies
-      const { data, error } = await supabase.auth.getSession();
-      if (error) throw error;
-      return data;
-    } else {
-      //If we are simulating logged in user, sign in with email and password to get session instead
-      //Reason: getSession() won't work because the Plasmic studio renders the app in an iframe
-      const validFields =
-        simulateUserSettings.email && simulateUserSettings.password;
-      if (!validFields)
-        throw new Error(
-          "You must provide an email and password to simulate a logged in user (Project settings -> SupabaseUser)"
-        );
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: simulateUserSettings.email as string,
-        password: simulateUserSettings.password as string,
-      });
-      if (error) throw error;
-      return data;
-    }
-  }, [simulateUserSettings]);
+  //   if (!simulateUserSettings.simulateLoggedInUser) {
+  //     //If we are NOT simulating logged in user, get session from localStorage / cookies
+  //     const { data, error } = await supabase.auth.getSession();
+  //     if (error) throw error;
+  //     return data;
+  //   } else {
+  //     //If we are simulating logged in user, sign in with email and password to get session instead
+  //     //Reason: getSession() won't work because the Plasmic studio renders the app in an iframe
+  //     const validFields =
+  //       simulateUserSettings.email && simulateUserSettings.password;
+  //     if (!validFields)
+  //       throw new Error(
+  //         "You must provide an email and password to simulate a logged in user (Project settings -> SupabaseUser)"
+  //       );
+  //     const { data, error } = await supabase.auth.signInWithPassword({
+  //       email: simulateUserSettings.email as string,
+  //       password: simulateUserSettings.password as string,
+  //     });
+  //     if (error) throw error;
+  //     return data;
+  //   }
+  // }, [simulateUserSettings]);
 
   //Callback to get the session and set state variable session to the result
-  const getSessionAndSaveToState = useCallback(async () => {
-    try {
-      const data = await getSession();
-      setSession({
-        email: data.session?.user.email || null,
-        role: data.session?.user.role || null,
-        user_metadata: data.session?.user.user_metadata || null
-      });
-      setError(null);
-    } catch (e) {
-      setError(getErrMsg(e))
-      return;
-    }
-  }, [getSession]);
+  // const getSessionAndSaveToState = useCallback(async () => {
+  //   try {
+  //     const data = await getSession();
+  //     setSession({
+  //       email: data.session?.user.email || null,
+  //       role: data.session?.user.role || null,
+  //       user_metadata: data.session?.user.user_metadata || null
+  //     });
+  //     setError(null);
+  //   } catch (e) {
+  //     setError(getErrMsg(e))
+  //     return;
+  //   }
+  // }, [getSession]);
 
   //Initially fetch the session and save as state
+  // useEffect(() => {
+  //   getSessionAndSaveToState();
+  // }, [getSessionAndSaveToState]);
+
+  //On initial load, set the session to state
   useEffect(() => {
-    getSessionAndSaveToState();
-  }, [getSessionAndSaveToState]);
+    supabaseBrowserClient().then(async (supabase) => {
+
+      console.log('initial load')
+      
+      // const { data, error } = await supabase.auth.getUser();
+      const { data, error } = await supabase.auth.getSession();
+      const user = data?.session?.user;
+      if (error) {
+        console.log('error in useEffect supabaseUserProvider')
+        console.error(error);
+        throw error;
+      }
+
+      console.log('INITIAL LOAD USER')
+      console.log(user)
+
+      console.log('INITIAL LOAD data')
+      console.log(data);
+
+      //Save the session to state
+      // setSession({
+      //   email: user?.email || null,
+      //   role: user?.role || null,
+      //   user_metadata: user?.user_metadata || null
+      // });
+      setSession(data?.session);
+
+    }).catch((e) => {
+      console.log('setting error message in useEffect')
+      setError(getErrMsg(e))
+    }) ;
+  }, [])
 
   //Global actions that can be called from plasmic studio
   const actions = useMemo(
     () => ({
       //Login
       login: async (email: string, password: string) => {
+        console.log('logging in...')
         try {
-          const supabase = await supabaseBrowserClient(simulateUserSettings);
+          const supabase = await supabaseBrowserClient();
           const { data, error } = await supabase.auth.signInWithPassword({
             email,
             password,
           });
-          if (error) throw error;
-          // await getSessionAndSaveToState();
-          //Seems to work where the above does not
-          setSession({
-            email: data?.user.email || null,
-            role: data?.user.role || null,
-            user_metadata: data?.user.user_metadata || null,
-          })
+          if (error) {
+            console.log('error in login')
+            console.error(error);
+            throw error;
+          }
+          
+          //Save the session to state
+          setSession(data?.session);
+          
+          //Save the access_token in localStorage
+          //This helps us to log in when viewing the app in Plasmic Studio / Plasmic studio preview
+          if(typeof window !== 'undefined') {
+            // window.localStorage.setItem('sb-token-plasmic-saved', data?.session?.access_token);
+            // window.localStorage.setItem('sb-session-plasmic-saved', JSON.stringify(data?.session));
+            window.localStorage.setItem('sb-custom-storage-key', JSON.stringify(data?.session));
+          }
+          
           setError(null);
           if(redirectOnLoginSuccess && router) router.push(redirectOnLoginSuccess);
+          
           return;
+
         } catch (e) {
+          console.log('setting error message in login')
           setError(getErrMsg(e))
           return;
         }
       },
       //Logout
       logout: async () => {
+        console.log('logging out...')
         try {
-          const supabase = await supabaseBrowserClient(simulateUserSettings);
+          const supabase = await supabaseBrowserClient();
           const { error } = await supabase.auth.signOut();
-          if (error) throw error;
-          await getSessionAndSaveToState();
+          if (error) {
+            console.log('error in logout')
+            console.error(error);
+            throw error;
+          }
+          // await getSessionAndSaveToState();
+
+          //Remove session from state
+          // setSession({
+          //   email: null,
+          //   role: null,
+          //   user_metadata: null
+          // })
+          setSession(null);
+
+          //Remove the access_token from localStorage
+          if(typeof window !== 'undefined') {
+            // window.localStorage.removeItem('sb-token-plasmic-saved');
+            window.localStorage.removeItem('sb-session-plasmic-saved');
+          }
+
           setError(null);
           return;
         } catch (e) {
+          console.log('setting error message in logout')
           setError(getErrMsg(e))
           return;
         }
       },
     }),
-    [getSessionAndSaveToState, simulateUserSettings, redirectOnLoginSuccess, router]
+    [redirectOnLoginSuccess, router]
   );
   
   //Setup the data that will be passed as global context to Plasmic studio
   const dataProviderData: DataProviderData = {
     user: session,
-    simulateUserSettings: {
-      simulateLoggedInUser: simulateLoggedInUser,
-      email: email,
-      password: password,
-    },
     error,
   };
 
--- utils/supabaseBrowserClient.ts
+++ utils/supabaseBrowserClient.ts
@@ -2,71 +2,133 @@ import { createBrowserClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/types/supabase";
 
-type SimulateUserSettings = {
-  simulateLoggedInUser: boolean;
-  email: string | null;
-  password: string | null;
-};
+// type SimulateUserSettings = {
+//   simulateLoggedInUser: boolean;
+//   email: string | null;
+//   password: string | null;
+// };
 
-const supabaseBrowserClient = async (
-  simulateUserSettings?: SimulateUserSettings
-) => {
+const supabaseBrowserClient = async () => {
   //Create the supabase client like normal
-  const supabase = createBrowserClient<Database>(
+  const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        storageKey: 'sb-custom-storage-key'
+      }
+    }
   );
+
+  return supabase;
 
   //Determine whether we're returning normal supabase client
   //Or using email/password values from the App Settings -> UserProvider to simulate a logged in user while working in the studio
   //Reason this needed: the app is inside an iframe when working in a studio, meaning localStorage & cookies do not work
   //So if we want to view login-protected data while working in the studio, we must simulate a user this way
   
-  const shouldSimulate =
-    process.env.NODE_ENV === "development" &&
-    simulateUserSettings &&
-    simulateUserSettings.simulateLoggedInUser ? true : false;
+  // const shouldSimulate =
+  //   process.env.NODE_ENV === "development" &&
+  //   simulateUserSettings &&
+  //   simulateUserSettings.simulateLoggedInUser ? true : false;
 
-  if (!shouldSimulate) {
+  //Check if there's an authenticated user via normal automatic methods
+  const { data, error } = await supabase.auth.getSession();
+  if (error) {
+    console.log('error in getSession')
+    console.error(error);
+    throw error;
+  }
+  
+  //If there is an authenticated user, return the normal supabase client
+  if (data.session) {
+    console.log('There is an authenticated user. Return 1')
+    console.log(data.session)
     return supabase;
+
+  //If there's not, it could be because the user is logged out, or could be because we're in plasmic studio or preview
   } else {
 
     //Check we have all required values
-    if (
-      !simulateUserSettings ||
-      !simulateUserSettings.email ||
-      !simulateUserSettings.password
-    )
-      throw new Error(
-        "You have elected to simulate a logged in user for studio development. However you have not provided an email or passowrd. Go to Project Settings -> SupabaseUser to fix."
-      );
+    // if (
+    //   !simulateUserSettings ||
+    //   !simulateUserSettings.email ||
+    //   !simulateUserSettings.password
+    // )
+    //   throw new Error(
+    //     "You have elected to simulate a logged in user for studio development. However you have not provided an email or passowrd. Go to Project Settings -> SupabaseUser to fix."
+    //   );
     
     //Sign in with email/password to get a token
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: simulateUserSettings.email,
-      password: simulateUserSettings.password,
-    });
+    // const { data, error } = await supabase.auth.signInWithPassword({
+    //   email: simulateUserSettings.email,
+    //   password: simulateUserSettings.password,
+    // });
 
-    if (error) throw error;
+    // if (error) throw error;
 
     //Extract the token
-    const token =  data?.session?.access_token;
+    // const token =  data?.session?.access_token;
 
-    if(!token) throw new Error('Could not get token from Supabase for simulated user.')
+    console.log('no authenticated user.')
 
-    //Create a native supabase client (supabase-js instead of @supabase/ssr) which lets us set custom headers
-    //Return that client for use in the studio
-    return createClient<Database>(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        global: {
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-        },
-      }
+    //Try to get the token from localStorage (manually saved there by SupabaseUserProvider)
+    // let token = null;
+    let sessionFromLocalStorage = null;
+    if(typeof window !== 'undefined') {
+      // token = window.localStorage.getItem('sb-token-plasmic-saved')
+      sessionFromLocalStorage = window.localStorage.getItem('sb-session-plasmic-saved')
+    };
+
+    // console.log('Token from localStorage is: ', token);
+    console.log('Session from localStorage is: ', sessionFromLocalStorage);
+
+    //If there's still not token, the user is truly not logged in, so return the normal supabase client
+    // if(!token) {
+    //   console.log('no token, return 2')
+    //   return supabase;
+    // }
+
+    if(!sessionFromLocalStorage) {
+      console.log('no session. Valid no logged in user. return 2')
+      return supabase;
+    }
+
+    // console.log('There is a token, creating manual client... Returning 3....')
+    console.log('There is a session, adding to supabsae client');
+
+    //If there is a token, then we're in the Plasmic Studio or preview, so we can use the token to create a client
+    //Here we create a supabase client using supabase-js instead of @supabase/ssr) which lets us set custom headers
+    //Return that client
+    // return createClient<Database>(
+    //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    //   {
+    //     global: {
+    //       headers: {
+    //         Authorization: `Bearer ${token}`
+    //       },
+    //     },
+    //   }
+    // );
+    const { data: setSessionData, error: setSessionErr } = await supabase.auth.setSession(
+      JSON.parse(sessionFromLocalStorage)
     );
+
+    if(setSessionErr) {
+      console.log('Error in setSession')
+      console.error(setSessionErr);
+      throw setSessionErr;
+    }
+
+    console.log('set session data')
+    console.log(setSessionData)
+
+    const {data: test} = await supabase.auth.getSession();
+    console.log('test')
+    console.log(test)
+
+    return supabase;
   }
 };
 
