1211045a17120dffaa873227b160553673a49782
CallumBoase <callum.boase@gmail.com>
2024-03-07 16:57:50 +1100

    middleware

---
+++ middleware.ts
@@ -0,0 +1,88 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+const protectedRoutes = ["/middlewareprotected"]; // the list of protected routes. This could be adapated to take and array from the GlobalContextProvider
+
+export async function middleware(request : NextRequest) {
+  // create a response that we can later modify the headers for
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+  // create the supabase client
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name, options) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // check some auth condition and redirect if not met - for now this is just whether the page is in protected array and whether the user is logged in
+  if (
+    !user &&
+    protectedRoutes?.some((path) => request.nextUrl.pathname.startsWith(path))
+  ) {
+    const redirectUrl = new URL("/login", request.nextUrl.origin);
+    return NextResponse.redirect(redirectUrl.toString());
+  }
+
+  return response;
+}
+
+// the matcher defines which routes the middleware is run on
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+};
