3520032c1a6f6e0c1786c2dd5d72cfbb57efe53b
CallumBoase <callum.boase@gmail.com>
2024-03-08 12:54:35 +1100

    Plasmic init

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -4,6 +4,7 @@ import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 import { SupabaseUser } from "./components/SupabaseUserProvider";
 import { SupabaseProvider } from "./components/SupabaseProvider";
 import { SupabaseAddRowProvider } from "./components/SupabaseAddRowProvider";
+import { SupabaseStorageProvider } from "./components/SupabaseStorageProvider";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -30,11 +31,10 @@ export const PLASMIC = initPlasmicLoader({
 
 // PLASMIC.registerComponent(...);
 
-//Works but makes building hard - only populated after login in PREVIEW or localhost not in builder
 PLASMIC.registerGlobalContext(SupabaseUser, {
   name: "SupabaseUserGlobalContext",
   props: {
-    redirectOnLoginSuccess: "string",
+      defaultRedirectOnLoginSuccess: "string",
   },
   providesData: true,
   globalActions: {
@@ -48,10 +48,51 @@ PLASMIC.registerGlobalContext(SupabaseUser, {
           name: "password",
           type: "string",
         },
+        {
+          name:"successRedirect",
+          type: "string"
+        }
+      ],
+    },
+    signup: {
+      parameters: [
+        {
+          name: "email",
+          type: "string",
+        },
+        {
+          name: "password",
+          type: "string",
+        },
+        {
+          name:"successRedirect",
+          type: "string"
+        }
       ],
     },
     logout: {
-      parameters: [],
+      parameters: [
+        {
+          name:"successRedirect",
+          type: "string"
+        }
+      ]
+    },
+    resetPasswordForEmail: {
+      parameters: [
+        {
+        name: "email",
+        type: "string",
+        },
+      ],
+    },
+    updateUserPassword: {
+      parameters: [
+        {
+        name: "password",
+        type: "string",
+        },
+      ],
     },
   },
 });
@@ -79,7 +120,7 @@ PLASMIC.registerComponent(SupabaseAddRowProvider, {
 });
 
 PLASMIC.registerComponent(SupabaseProvider, {
-  name: "SupabaseProvider",
+  name: "Supabase Data Provider",
   providesData: true,
   props: {
     queryName: {
@@ -100,7 +141,99 @@ PLASMIC.registerComponent(SupabaseProvider, {
         type: "object",
         fields: {
           fieldName: "string",
-          operator: "string",
+          operator: {
+            type: "choice",
+            options: [
+              {
+                value: "eq",
+                label: "is equal to (eq)"
+              },
+              {
+                value: "neq",
+                label: "not equal to (neq)"
+              },
+              {
+                value: "gt",
+                label: "greater than (gt)"
+              },
+              {
+                value: "gte",
+                label: "greater than or equal to (gte)"
+              },
+              {
+                value: "lt",
+                label: "less than (lt)"
+              },
+              {
+                value: "lte",
+                label: "less than or equal to (lte)"
+              },
+              {
+                value: "like",
+                label: "matches a case-sensitive pattern (like)"
+              },
+              {
+                value: "ilike",
+                label: "matches a case-insensitive pattern (ilike)"
+              },
+              {
+                value: "is",
+                label: "is (is)"
+              },
+              {
+                value: "in",
+                label: "is in an array (in)"
+              },
+              {
+                value: "contains",
+                label: "contains every element in (contains)"
+              },
+              {
+                value: "containedby",
+                label: "contained by (containedby)"
+              },
+              {
+                value: "rangeGt",
+                label: "greater than range (rangeGt)"
+              },
+              {
+                value: "rangeGte",
+                label: "greater than or equal to range (rangeGte)"
+              },
+              {
+                value: "rangeLt",
+                label: "less than range (rangeLt)"
+              },
+              {
+                value: "rangeLte",
+                label: "less than or equal to range (rangeLte)"
+              },
+              {
+                value: "rangeAdjacent",
+                label: "is mutually exclusive to range (rangeAdjacent)"
+              },
+              {
+                value: "overlaps",
+                label: "has an element in common with (overlaps)"
+              },
+              {
+                value: "match",
+                label: "where each { column:value, ... } matches (match)"
+              },
+              {
+                value: "or",
+                label: "that matches at least one PostgREST filter (or)"
+              },
+              {
+                value: "textSearch",
+                label: "matches the query string (textSearch)"
+              },
+              {
+                value: "not",
+                label: "that doesn't match the PostgREST filter (not)"
+              },
+            ]
+          },
           value: "string",
           value2: "string",
         },
@@ -203,15 +336,15 @@ PLASMIC.registerComponent(SupabaseProvider, {
     addRow: {
       description: "add a row",
       argTypes: [
-        { name: "rowForSupabase", type: "object", displayName: "Row to send to Supabase" },
-        { name: "optimisticRow", type: "object", displayName: "Optimistic new row (optional)"},
+        { name: "rowForSupabase", type: "object", displayName: "Row object to send to Supabase" },
+        { name: "optimisticRow", type: "object", displayName: "Optimistic new row object (optional)"},
       ],
     },
     editRow: {
       description: "edit row",
       argTypes: [
-        { name: "rowForSupabase", type: "object", displayName: "Row to send to Supabase"},
-        { name: "optimisticRow", type: "object", displayName: "Optimistic edited row (optional)"},
+        { name: "rowForSupabase", type: "object", displayName: "Row object to send to Supabase"},
+        { name: "optimisticRow", type: "object", displayName: "Optimistic edited row object (optional)"},
       ],
     },
     runRpc: {
@@ -235,3 +368,91 @@ PLASMIC.registerComponent(SupabaseProvider, {
     },
   },
 });
+
+PLASMIC.registerComponent(SupabaseStorageProvider, {
+  name: "SupabaseStorageProvider",
+  providesData: true,
+  props: {
+    children: {
+      type: "slot"
+    },
+    instanceName: {
+      type: "string"
+    },
+    bucketName: {
+      type: "string"
+    }
+  },
+  refActions: {
+    uploadFile : {
+      description: "Uploads a file to an existing bucket.",
+      argTypes: [
+        { name: "path", type: "string", displayName: "Upload path (including file name)"},
+        { name: "base64FileData", type: "string", displayName: "File data (base64 encoded string)"},
+        { name: "contentType", type: "string", displayName: "Content Type / MIME type"},
+        { name: "upsert", type: "boolean", displayName: "Allow overwrite (optional). Default = false."}
+        // cacheControl property has been intentionally not included
+      ]
+    },
+    uploadManyFiles : {
+      description: "Upload a list of files to an existing bucket (assumes array of objects with shape { name: 'filename.ext', type: 'MIME type', contents: 'base64string'}",
+      argTypes: [
+        { name: "fileDataList", type: "array", displayName: "Array of objects with shape { name: 'filename.ext', type: 'MIME type', contents: 'base64string'}" }, 
+        { name: "folder", type: "string", displayName: "The folder/prefix to store each file in." }, 
+        { name: "upsert", type: "boolean", displayName: "Allow overwrite (optional). Default = false." },
+        { name: "replaceFilename", type: "boolean", displayName: "Replace filename with UUID"}
+      ]
+    },
+    downloadFile : {
+      description: "Downloads a file from a private bucket. For public buckets, make a request to the URL returned from 'getPublicUrl' instead or derive the URL.",
+      argTypes: [
+        { name: "path", type: "string", displayName: "Path and filename to download"},
+        { name: "optimization", type: "boolean", displayName: "Automatic image optimization (WebP) (optional). Default = true."},
+        // other transform properties have been intentinoally not included as they are currently in beta/require pro or enterprise tier supabase
+      ]
+    },
+    replaceFile : {
+      description: "Replaces an existing file at the specified path with a new one.",
+      argTypes: [
+        { name: "path", type: "string", displayName: "Upload path (including file name)"},
+        { name: "base64FileData", type: "string", displayName: "File data (base64 encoded string)"},
+        { name: "contentType", type: "string", displayName: "Content Type / MIME type"},
+        { name: "upsert", type: "boolean", displayName: "Allow overwrite (optional). Default = false."}
+      ]
+    },
+    moveFile : {
+      description: "Moves an existing file to a new path in the same bucket.",
+      argTypes: [
+        { name: "fromPath", type: "string", displayName: "From path (including current file name)"},
+        { name: "toPath", type: "string", displayName: "To data (including new file name)"},
+      ]
+    },
+    copyFile : {
+      description: "Copies an existing file to a new path in the same bucket.",
+      argTypes: [
+        { name: "fromPath", type: "string", displayName: "From path (including current file name)"},
+        { name: "toPath", type: "string", displayName: "To data (including new file name)"},
+      ]
+    },
+    deleteFiles : {
+      description: "Deletes specified files within the same bucket",
+      argTypes: [
+        { name: "paths", type: "array", displayName: "Paths to delete (array of paths, including current file name)"},
+      ]
+    },
+    listFiles: {
+      description: "Lists all the files within a bucket.",
+      argTypes: [
+        { name: "path", type: "string", displayName: "Folder path (optional)"},
+        { name: "limit", type: "number", displayName: "Number of files to return (optional). Default = 100"},
+        { name: "offset", type: "number", displayName: "Offset/Starting position (optional). Default = 0" },
+        { name: "sortBy", type: "object", displayName: "Sort by column (optional). Object like { column: 'name', order: 'asc' }" },
+        { name: "search", type: "string", displayName: "Search string to filter files by (optional)" },
+      ]
+    },
+    emptyBucket : {
+      description: "Removes all objects inside a single bucket.",
+      argTypes: []
+    },
+  },
+})
